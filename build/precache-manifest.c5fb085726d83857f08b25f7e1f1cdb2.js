db72ff53fd928c672d8fdb9caad1af702506765b
traysimay <teh@raptor.pizza>
2020-05-16 17:58:31 -0700

    some party

---
+++ build/precache-manifest.c5fb085726d83857f08b25f7e1f1cdb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b76f560fd6efeec49e2acb5cd293b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7be53229c0d82f127b2b",
+    "url": "/static/css/main.af7ff11c.chunk.css"
+  },
+  {
+    "revision": "0f43e5d6e9116ad9b866",
+    "url": "/static/js/2.6e478d55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6e478d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be53229c0d82f127b2b",
+    "url": "/static/js/main.74283392.chunk.js"
+  },
+  {
+    "revision": "089a865c9774b9694f8f",
+    "url": "/static/js/runtime-main.8de0129c.js"
+  },
+  {
+    "revision": "2e6fafa02cb012e877488201ba980df3",
+    "url": "/static/media/Stolzl Display.2e6fafa0.otf"
+  }
+]);
