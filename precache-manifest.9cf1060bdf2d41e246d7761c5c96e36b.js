49095c5b202afef1af8a656b840e46a0fe13f0bd
traysimay <teh@raptor.pizza>
2020-12-12 11:47:10 -0800

    Updates

---
+++ precache-manifest.9cf1060bdf2d41e246d7761c5c96e36b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711a82250c5ad7228235343481ee3f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41ba9c6bad6058b1138a",
+    "url": "/static/css/main.af7ff11c.chunk.css"
+  },
+  {
+    "revision": "de2784430469ed6f953a",
+    "url": "/static/js/2.e9b0f319.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e9b0f319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ba9c6bad6058b1138a",
+    "url": "/static/js/main.9034049f.chunk.js"
+  },
+  {
+    "revision": "089a865c9774b9694f8f",
+    "url": "/static/js/runtime-main.8de0129c.js"
+  },
+  {
+    "revision": "2e6fafa02cb012e877488201ba980df3",
+    "url": "/static/media/Stolzl Display.2e6fafa0.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4380f6197ba8212430cdc615d8708d2c.js"
+  "/precache-manifest.9cf1060bdf2d41e246d7761c5c96e36b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
